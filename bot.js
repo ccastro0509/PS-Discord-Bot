cb55e02d7f010b9cc2a43a0d2fe5ca67ba10b974
PartMan7 <47669599+PartMan7@users.noreply.github.com>
2019-07-11 17:55:45 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,10 +2,11 @@ let config = require("./config.js");
 let SDClient = require('./client.js');
 let Discord = require('discord.js');
 let globaljs = require('./global.js');
+let globalranks = require('./ranks.js');
 
 //Check if details have been entered
-if (config.prefix === 'PREFIX' || config.nickName === 'USERNAME' || config.pass === 'PASSWORD' || config.token === 'TOKEN' || config.owner === 'OWNER') {
-  console.log('Please fill in the required details in config.js. (prefix, nickName, pass, token, owner)');
+if (config.prefix === 'PREFIX' || config.nickName === 'USERNAME' || config.pass === 'PASSWORD' || config.token === 'TOKEN' || config.owner === 'OWNER' || !admin[0]) {
+  console.log('Please fill in the required details in config.js (prefix, nickName, pass, token, owner) and in ranks.js (admin).');
   process.exit();
 }
 
@@ -85,65 +86,78 @@ if (useDiscord) {
 Bot.on('connect', function (connection) {
   console.log('Connected to Pokémon Showdown.');
 });
-  config.admin.forEach(function(element) {
-    admin.push(toId(element));
-  });
-  config.alpha.forEach(function(element) {
-    alpha.push(toId(element));
-  });
-  config.beta.forEach(function(element) {
-    beta.push(toId(element));
-  });
-  config.gamma.forEach(function(element) {
-    gamma.push(toId(element));
-  });
-  config.locked.forEach(function(element) {
-    locked.push(toId(element));
-  });
-  config.logrooms.forEach(function(element) {
-    logRooms.push(toId(element));
-  });
 Bot.on('chat', function (room, time, by, message) {
-  if (logRooms.includes(room)) {
-      fs.appendFile(`./chatlogs/${room}.txt`, `\n${DateString()} | ${by}: ${message}`, function (err) {
-        if (err) console.log(err);
-      });
-  }
-  if (message === `${config.nickName}?`) return Bot.say(room, `Hi, I'm ${config.nickName}! My prefix is \`\`${config.prefix}\`\`. I hope to be of assistance! If there're any problems, please contact my owner: ${owner}.`);
+  if (message === `${config.nickName}?`) return Bot.say(room, `Hi, I'm ${config.nickName}! My prefix is \`\`${config.prefix}\`\`. I hope to be of assistance!`);
   if (locked.includes(toId(by))) return;
-  //if (message.startsWith('Hello')) return Bot.say(room, 'Hi!');
     if (!message.startsWith(prefix)) return;
     messagecontent = message.substr((prefix).length);
     let args = messagecontent.split(' ');
     let command = args.shift().toLowerCase();
-    if (by.startsWith('~') || by.startsWith('&') || by.startsWith('#') || by.startsWith('@')) {
-      if (!admin.includes(toId(by)) && !alpha.includes(toId(by))) alpha.push(toId(by));
+    if (by.startsWith('~') || by.startsWith('&') || by.startsWith('#') || by.startsWith('*') || by.startsWith('@')) {
+      if (!admin.includes(toId(by)) && !alpha.includes(toId(by))) {
+        alpha.push(toId(by));
+        spliceRank = by.charAt(0);
+      }
     }
     if (by.startsWith('%')) {
-      if (!admin.includes(toId(by)) && !alpha.includes(toId(by)) && !beta.includes(toId(by))) beta.push(toId(by));
+      if (!admin.includes(toId(by)) && !alpha.includes(toId(by)) && !beta.includes(toId(by))) {
+        beta.push(toId(by));
+        spliceRank = by.charAt(0);
+      }
     }
     if (by.startsWith('+')) {
-      if (!admin.includes(toId(by)) && !alpha.includes(toId(by)) && !beta.includes(toId(by)) && !gamma.includes(toId(by))) gamma.push(toId(by));
+      if (!admin.includes(toId(by)) && !alpha.includes(toId(by)) && !beta.includes(toId(by)) && !gamma.includes(toId(by))) {
+        gamma.push(toId(by));
+        spliceRank = by.charAt(0);
+      }
     }
     if (command === 'help') {
-      if (!args[0] || args[0].toLowerCase() === 'help') return Bot.say(room, 'Uhh, what do you need help with?');
+      if (!args[0] || args[0].toLowerCase() === 'help') {
+        if (spliceRank) spliceRanks(spliceRank, by);
+        return Bot.say(room, 'Uhh, what do you need help with?');
+      }
       fs.readdir(cfol, (e, files) => {
-        if (args[0].toLowerCase() === 'eval') return Bot.say(room, 'Admin stuff. Shh.');
-        if (!files.includes(`${args[0].toLowerCase()}.js`)) return Bot.say(room, 'Command not found.');
+        if (args[0].toLowerCase() === 'eval') {
+          if (spliceRank) spliceRanks(spliceRank, by);
+          return Bot.say(room, 'Admin stuff. Shh.');
+        }
+        if (!files.includes(`${args[0].toLowerCase()}.js`)) {
+          if (spliceRank) spliceRanks(spliceRank, by);
+          return Bot.say(room, 'Command not found.');
+        }
         let commandFile = require(`./commands/${args[0].toLowerCase()}.js`);
-        if (commandFile.help == undefined) return Bot.say(room, 'Help for this command was not found.');
-        if (commandFile.permissions === 'admin' && !admin.includes(toId(by))) return Bot.say(room, 'Admin stuff. Shh.');
-        if (commandFile.permissions === 'alpha' && !admin.includes(toId(by)) && !alpha.includes(toId(by))) return Bot.say(room, 'Alpha stuff. Shh.');
-        if (commandFile.permissions === 'beta' && !admin.includes(toId(by)) && !alpha.includes(toId(by)) && !beta.includes(toId(by))) return Bot.say(room, 'Beta stuff. Shh.');
-        if (commandFile.permissions === 'gamma' && !admin.includes(toId(by)) && !alpha.includes(toId(by)) && !beta.includes(toId(by)) && !gamma.includes(toId(by))) return Bot.say(room, 'Gamma stuff. Shh.');
-        return Bot.say(room, commandFile.help);
+        if (commandFile.help == undefined) {
+          if (spliceRank) spliceRanks(spliceRank, by);
+          return Bot.say(room, 'Help for this command was not found.');
+        }
+        if (commandFile.permissions === 'admin' && !admin.includes(toId(by))) {
+          if (spliceRank) spliceRanks(spliceRank, by);
+          return Bot.say(room, 'Admin stuff. Shh.');
+        }
+        if (commandFile.permissions === 'alpha' && !admin.includes(toId(by)) && !alpha.includes(toId(by))) {
+          if (spliceRank) spliceRanks(spliceRank, by);
+          return Bot.say(room, 'Alpha stuff. Shh.');
+        }
+        if (commandFile.permissions === 'beta' && !admin.includes(toId(by)) && !alpha.includes(toId(by)) && !beta.includes(toId(by))) {
+          if (spliceRank) spliceRanks(spliceRank, by);
+          return Bot.say(room, 'Beta stuff. Shh.');
+        }
+        if (commandFile.permissions === 'gamma' && !admin.includes(toId(by)) && !alpha.includes(toId(by)) && !beta.includes(toId(by)) && !gamma.includes(toId(by))) {
+          if (spliceRank) spliceRanks(spliceRank, by);
+          return Bot.say(room, 'Gamma stuff. Shh.');
+        }
+        Bot.say(room, commandFile.help);
+        if (spliceRank) spliceRanks(spliceRank, by);
       });
       return;
     }
     if (command === 'eval') {
       if (!admin.includes(toId(by))) return Bot.say(room, 'Access denied.');
       try {
-        Bot.say(room, eval(args.join(' ')));
+        let eoutpu = eval(args.join(' '));
+        if (typeof(eoutpu) === 'object') return Bot.say(room, JSON.stringify(eoutpu));
+        else if (typeof(eoutpu) === 'function') return Bot.say(room, '!code \n' + eoupu.toString());
+        else Bot.say(room, eoutpu);
       }
       catch (e) {
         Bot.say(room, e.message);
@@ -152,31 +166,52 @@ Bot.on('chat', function (room, time, by, message) {
       return;
     }
     fs.readdir(cfol, (e, files) => {
-      if (!files.includes(`${command}.js`)) return Bot.say(room, 'Command not found.');
+      if (!files.includes(`${command}.js`)) {
+        if (spliceRank) spliceRanks(spliceRank, by);
+        return Bot.say(room, 'Command not found.');
+      }
       let commandFile = require(`./commands/${command}.js`);
       if (commandFile.permissions === 'admin') {
+        if (spliceRank) spliceRanks(spliceRank, by);
         if (!admin.includes(toId(by))) return Bot.say(room, 'Access denied.');
-        return commandFile.commandFunction(Bot, room, by, args, client);
+        commandFile.commandFunction(Bot, room, by, args, client);
       }
-      if (commandFile.permissions === 'alpha') {
-        if (!admin.includes(toId(by)) && !alpha.includes(toId(by))) return Bot.say(room, 'Access denied.');
-        return commandFile.commandFunction(Bot, room, by, args, client);
+      else if (commandFile.permissions === 'alpha') {
+        if (!admin.includes(toId(by)) && !alpha.includes(toId(by))) {
+          if (spliceRank) spliceRanks(spliceRank, by);
+          return Bot.say(room, 'Access denied.');
+        }
+        commandFile.commandFunction(Bot, room, by, args, client);
+        if (spliceRank) spliceRanks(spliceRank, by);
       }
-      if (commandFile.permissions === 'beta') {
-        if (!admin.includes(toId(by)) && !alpha.includes(toId(by)) && !beta.includes(toId(by))) return Bot.say(room, 'Access denied.');
-        return commandFile.commandFunction(Bot, room, by, args, client);
+      else if (commandFile.permissions === 'beta') {
+        if (!admin.includes(toId(by)) && !alpha.includes(toId(by)) && !beta.includes(toId(by))) {
+          if (spliceRank) spliceRanks(spliceRank, by);
+          return Bot.say(room, 'Access denied.');
+        }
+        commandFile.commandFunction(Bot, room, by, args, client);
+        if (spliceRank) spliceRanks(spliceRank, by);
       }
-      if (commandFile.permissions === 'gamma') {
-        if (!admin.includes(toId(by)) && !alpha.includes(toId(by)) && !beta.includes(toId(by)) && !gamma.includes(toId(by))) return Bot.say(room, 'Access denied.');
-        return commandFile.commandFunction(Bot, room, by, args, client);
+      else if (commandFile.permissions === 'gamma') {
+        if (!admin.includes(toId(by)) && !alpha.includes(toId(by)) && !beta.includes(toId(by)) && !gamma.includes(toId(by))) {
+          if (spliceRank) spliceRanks(spliceRank, by);
+          return Bot.say(room, 'Access denied.');
+        }
+        commandFile.commandFunction(Bot, room, by, args, client);
+        if (spliceRank) spliceRanks(spliceRank, by);
       }
-      if (commandFile.permissions === 'none') {
-        return commandFile.commandFunction(Bot, room, by, args, client);
+      else if (commandFile.permissions === 'none') {
+        commandFile.commandFunction(Bot, room, by, args, client);
+        if (spliceRank) spliceRanks(spliceRank, by);
+      }
+      else {
+        if (spliceRank) spliceRanks(spliceRank, by);
+        return Bot.say(room, 'Error occurred.');
       }
     });
 });  
 Bot.on('pm', function (by, message) {
-  if (!admin.includes(toId(by))) Bot.pm(owner, `${by}: ${message}`);
+  if (!admin.includes(toId(by))) Bot.pm('PartMan', `${by}: ${message}`);
   let mtoid = toId(message);
   if (mtoid.indexOf('invit') !== -1) Bot.pm(by, `/invite ${tcroom}`);
     if (!message.startsWith(prefix)) return;
@@ -210,7 +245,16 @@ Bot.on('pm', function (by, message) {
     }
     if (command === 'j' && args[0]) Bot.pm(by, `/j ${args.join(' ')}`);
 });
-
+Bot.on('error', function(room, emessage) {
+  let str = room + ': ' + emessage;
+  if (str.length > 300) str = str.substr(0,300);
+  Bot.pm('PartMan', str);
+});
+Bot.on('popup', function(pmessage) {
+  if (pmessage.length > 293) pmessage = pmessage.substr(0, 293);
+  Bot.pm('PartMan', 'Popup: ' + pmessage);
+});
+Bot.on('queryresponse', function(response) {Bot.say(queryRoom, response);});
 let standard_input = process.stdin;
 standard_input.setEncoding('utf-8');
 standard_input.on('data', function(data) {
