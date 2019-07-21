ae92cc03d5028bcbe1389efd09cb1f71f27d676e
ccastro0509 <52643272+ccastro0509@users.noreply.github.com>
2019-07-20 19:41:11 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
-exports.prefix = "PREFIX"; //Command prefix.
-exports.nickName = "USERNAME"; //Bot name.
+exports.prefix = "PREFIX";
+exports.nickName = "Lurien";
 exports.pass = 'PASSWORD'; 
-exports.token = 'TOKEN'; //Leave '' to disable Discord.
-exports.avatar = 'supernerd'; //Bot's default avatar.
-exports.discordActivity = ''; //Activity that will be displayed on Discord. Leave '' to disable.
-exports.discordAdmin = []; //Discord ID of administrator(s). [string]
-exports.owner = 'OWNER'; //Owner of the Bot. Does NOT give admin permissions.
-exports.autoJoin = ['botdevelopment']; //Rooms that the Bot will automatically join.
-exports.logrooms = []; //Rooms that will have their chats logged. Bot must be in these rooms to log. Please seek permission from room staff before adding here.
+exports.token = 'TOKEN';
+exports.avatar = 'cilan';
+exports.discordActivity = '';
+exports.discordAdmin = [];
+exports.owner = 'Salient';
+exports.autoJoin = ['lobby'];
+exports.logrooms = [];
 
-exports.server = 'sim.smogon.com'; //Don't touch these unless you know what you're doing.
+exports.server = 'sim.smogon.com';
 exports.port = 8000;
 exports.serverid = 'showdown';
